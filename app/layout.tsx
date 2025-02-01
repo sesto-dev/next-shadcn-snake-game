a5f30a269446e4d207cc5afa9ec4af9122821f70
Amirhossein Mohammadi <accretence@outlook.com>
2025-02-01 23:32:55 +0330

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-}
+import type { Metadata } from "next";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
